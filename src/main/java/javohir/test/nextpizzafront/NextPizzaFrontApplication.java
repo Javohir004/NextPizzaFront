a9009fa203d5ee4a8cff7cfa93b76bf735dcb3c5
Javohir new <javohirsharifjonov795@gmail.com>
2026-02-10 15:51:18 +0500

    Initial commit

---
+++ src/main/java/javohir/test/nextpizzafront/NextPizzaFrontApplication.java
@@ -0,0 +1,13 @@
+package javohir.test.nextpizzafront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextPizzaFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextPizzaFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javohir/test/nextpizzafront/NextPizzaFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package javohir.test.nextpizzafront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextPizzaFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
